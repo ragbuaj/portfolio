4bca5ec8134e2e4436ab004b7232ac24602ff566
Ragil Budhi Ajie <ragbuaj@gmail.com>
2025-12-11 18:11:55 +0700

    fix: vercel speed-insight

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { injectSpeedInsights } from '@vercel/speed-insights'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+injectSpeedInsights()
 
 app.use(createPinia())
 app.use(router)
