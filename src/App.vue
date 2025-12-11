4e48f43e86aba83e49f88f8f09d20c954919613e
Ragil Budhi Ajie <ragbuaj@gmail.com>
2025-12-11 18:10:04 +0700

    feat: add @vercel/speed-insights dependency and integrate component in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 // import { RouterLink, RouterView } from 'vue-router'
 // import HelloWorld from './components/HelloWorld.vue'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 </script>
 
 <template>
+  <SpeedInsights />
   <header>
     <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
